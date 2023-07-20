006e6571d114354ecdded05e69b94a0b08baaa6a
Jazz Zeng <jazz.zeng@gmail.com>
2023-07-21 00:06:24 +1000

    register and cancel logic functions

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def register():
         password = request.form['password']
         if username and password:
             if username not in users:
-                users[username] = password
+                users[username] = {'password': password, 'registered_counters': []}
                 return jsonify({'message': 'success'})
             else:
                 return jsonify({'message': 'Username already exists'})
@@ -64,56 +64,75 @@ def logout():
 def increment_counter():
     counter_id = request.json['counterId']
     username = session.get('username')
-    if counter_id in counters:
-        if username not in counters[counter_id]['registered_users']:
-            counters[counter_id]['value'] += 1
-            counters[counter_id]['registered_users'].append(username)
-            return jsonify({'counterValue': counters[counter_id]['value'], 'username': session.get('username')})
-        else:
-            message = f'You have already registered for Counter {counter_id}'
-            return jsonify({'message': message})
+
+    # Check if the user is already registered for any counter
+    already_registered = any(username in counter['registered_users'] for counter in counters.values())
+
+    if not already_registered:
+        counters[counter_id]['value'] += 1
+        counters[counter_id]['registered_users'].append(username)
+        registered_users = counters[counter_id]['registered_users']
+        message = f'You are now registered on Court {counter_id}'
     else:
-        return jsonify({'message': 'Invalid counter'})
+        registered_users = counters[counter_id]['registered_users']
+        message = f'You have already registered for Counter {counter_id}'
+        
+    return jsonify({
+                'counterValue': counters[counter_id]['value'],
+                'message': message,
+                'usernames': registered_users  # Pass the list of usernames to the frontend
+            })
 
 @app.route('/decrement', methods=['POST'])
 def decrement_counter():
     counter_id = request.json['counterId']
     username = session.get('username')
-    if counter_id in counters:
-        if username in counters[counter_id]['registered_users']:
-            counters[counter_id]['value'] -= 1
-            counters[counter_id]['registered_users'].remove(username)
-            return jsonify({'counterValue': counters[counter_id]['value'], 'username': session.get('username')})
-        else:
-            message = f'You are not registered for Counter {counter_id}'
-            return jsonify({'message': message})
+    if username in counters[counter_id]['registered_users']:
+        counters[counter_id]['value'] -= 1
+        counters[counter_id]['registered_users'].remove(username)
+        registered_users = counters[counter_id]['registered_users']
+        message = f'You canceled registeration on Court {counter_id}, feel free to register to other courts'
     else:
-        return jsonify({'message': 'Invalid counter'})
+        registered_users = counters[counter_id]['registered_users']
+        message = f'You are not registered for this court'
+
+    return jsonify({
+                'counterValue': counters[counter_id]['value'],
+                'message': message,
+                'usernames': registered_users  # Pass the list of usernames to the frontend
+            })
 
 @app.route('/clear', methods=['POST'])
 def clear_four_users():
     counter_id = request.json['counterId']
     username = session.get('username')
-    if username in counters[counter_id]['registered_users']:
-        registered_users = counters[counter_id]['registered_users']
+
+    # Check if the user is already registered for any counter
+    registered_users = counters[counter_id]['registered_users']
+    already_registered = username in registered_users
+
+    if already_registered:
         if len(registered_users) >= 4:
             first_four_users = registered_users[:4]
             if username in first_four_users:
                 counters[counter_id]['value'] -= 4
                 counters[counter_id]['registered_users'] = registered_users[4:]
+                registered_users = counters[counter_id]['registered_users']
                 for user in first_four_users:
                     users[user]['registered_counters'].remove(counter_id)
                 message = "Counter cleared successfully. You can now register for any counter again."
-                return jsonify({'counterValue': counters[counter_id]['value'], 'username': session.get('username'), 'message': message})
             else:
                 message = "You are not one of the first four registered users"
-                return jsonify({'message': message})
         else:
             message = "Not enough registered users to clear the counter"
-            return jsonify({'message': message})
     else:
         message = f"You are not registered for Counter {counter_id}"
-        return jsonify({'message': message})
+    return jsonify({
+            'counterValue': counters[counter_id]['value'],
+            'message': message,
+            'usernames': registered_users  # Pass the list of usernames to the frontend
+        })
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -7,9 +7,28 @@ $(document).ready(function() {
             data: JSON.stringify({ counterId: counterId }),
             contentType: 'application/json',
             success: function(response) {
-                updateCounterValue(counterId, response.counterValue);
-                updateRegisteredUsers(counterId, response.username);
-                updateButtonStates(counterId);
+                // Display the counter value after incrementing
+                $("#counter-value-" + counterId).text(response.counterValue);
+                updateRegisteredUsers(counterId, response.usernames);
+
+                // Display the flash message if present
+                if (response.message) {
+                    Toastify({
+                        text: response.message,
+                        duration: 2000, // 2 seconds
+                        close: true,
+                        gravity: 'top', // You can choose 'top', 'bottom', 'left', 'right'
+                        position: 'center', // You can choose 'left', 'center', 'right'
+                        backgroundColor: 'linear-gradient(to right, #00b09b, #96c93d)',
+                        stopOnFocus: true, // Hide the toast on hover
+                      }).showToast();
+                }
+                
+                //updateButtonStates(counterId);
+            },
+            error: function(error) {
+                // Handle error if needed
+                console.log(error);
             }
         });
     });
@@ -22,9 +41,28 @@ $(document).ready(function() {
             data: JSON.stringify({ counterId: counterId }),
             contentType: 'application/json',
             success: function(response) {
-                updateCounterValue(counterId, response.counterValue);
-                updateRegisteredUsers(counterId, response.username);
-                updateButtonStates(counterId);
+                // Display the counter value after incrementing
+                $("#counter-value-" + counterId).text(response.counterValue);
+
+                // Display the flash message if present
+                if (response.message) {
+                // You can use any logic to show the flash message, e.g., alert, toast, etc.
+                Toastify({
+                    text: response.message,
+                    duration: 2000, // 2 seconds
+                    close: true,
+                    gravity: 'top', // You can choose 'top', 'bottom', 'left', 'right'
+                    position: 'center', // You can choose 'left', 'center', 'right'
+                    backgroundColor: 'linear-gradient(to right, #00b09b, #96c93d)',
+                    stopOnFocus: true, // Hide the toast on hover
+                  }).showToast();
+            }
+                updateRegisteredUsers(counterId, response.usernames);
+                //updateButtonStates(counterId);
+            },
+            error: function(error) {
+                // Handle error if needed
+                console.log(error);
             }
         });
     });
@@ -37,9 +75,28 @@ $(document).ready(function() {
             data: JSON.stringify({ counterId: counterId }),
             contentType: 'application/json',
             success: function(response) {
-                updateCounterValue(counterId, response.counterValue);
-                updateRegisteredUsers(counterId, response.username);
-                updateButtonStates(counterId);
+                // Display the counter value after incrementing
+                $("#counter-value-" + counterId).text(response.counterValue);
+
+                // Display the flash message if present
+                if (response.message) {
+                // You can use any logic to show the flash message, e.g., alert, toast, etc.
+                Toastify({
+                    text: response.message,
+                    duration: 2000, // 2 seconds
+                    close: true,
+                    gravity: 'top', // You can choose 'top', 'bottom', 'left', 'right'
+                    position: 'center', // You can choose 'left', 'center', 'right'
+                    backgroundColor: 'linear-gradient(to right, #00b09b, #96c93d)',
+                    stopOnFocus: true, // Hide the toast on hover
+                  }).showToast();
+            }
+                updateRegisteredUsers(counterId, response.usernames);
+                //updateButtonStates(counterId);
+            },
+            error: function(error) {
+                // Handle error if needed
+                console.log(error);
             }
         });
     });
@@ -48,19 +105,19 @@ $(document).ready(function() {
         $('#counter-value-' + counterId).text(value);
     }
 
-    function updateRegisteredUsers(counterId, username) {
+    function updateRegisteredUsers(counterId, usernames) {
         var registeredUsers = $('#registered-users-' + counterId);
-        var currentCount = parseInt(registeredUsers.text());
-        if (username === sessionUsername) {
-            if (!registeredUsers.hasClass('registered')) {
-                registeredUsers.addClass('registered');
-                registeredUsers.text(currentCount + 1);
-            }
+
+        // Clear the current content
+        registeredUsers.empty();
+
+        if (usernames.length > 0) {
+            // If there are registered users, concatenate their usernames
+            var userList = usernames.join(', ');
+            registeredUsers.text(userList);
         } else {
-            if (registeredUsers.hasClass('registered')) {
-                registeredUsers.removeClass('registered');
-                registeredUsers.text(currentCount - 1);
-            }
+            // If no users are registered, display a message
+            registeredUsers.text('No registered users');
         }
     }
 
@@ -68,15 +125,20 @@ $(document).ready(function() {
         var registeredUsers = parseInt($('#registered-users-' + counterId).text());
         var decrementButton = $('#counter-' + counterId + ' .decrement');
         var clearButton = $('#counter-' + counterId + ' .clear');
-        if (registeredUsers >= 4) {
-            clearButton.prop('disabled', false);
-        } else {
-            clearButton.prop('disabled', true);
-        }
-        if (decrementButton.hasClass('registered')) {
+
+        var currentCounter = counters[counterId]['registered_users'];
+        var isRegistered = currentCounter.includes(sessionUsername);
+
+        if (isRegistered) {
             decrementButton.prop('disabled', false);
-        } else {
+        }
+        else {
             decrementButton.prop('disabled', true);
+            if (registeredUsers >= 4) {
+                clearButton.prop('disabled', false);
+            } else {
+                clearButton.prop('disabled', true);
+            }
         }
     }
 });
